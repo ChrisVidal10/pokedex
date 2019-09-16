36f4eaf643e62d80dfc09005fa4e08111eb96c1a
cvidal <chris.vidal10@gmail.com>
2019-09-15 21:46:29 -0300

    Types of pokemons implementation

--- pokedex/schema.py
+++ pokedex/schema.py
@@ -1,12 +1,14 @@
 from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
 
-from pokedex.nodes import PokemonNode
+from pokedex.nodes import PokemonNode, TypeNode
 
 
 class Query(object):
     pokemon = relay.Node.Field(PokemonNode)
     pokemons = DjangoFilterConnectionField(PokemonNode)
+    type = relay.Node.Field(TypeNode)
+    types = DjangoFilterConnectionField(TypeNode)
 
 
 class Mutation(object):
